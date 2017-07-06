7da3ece2287346a935198d939de2a31d788664d5
Peter Fonseca <pfonseca@23andme.com>
2017-07-05 17:05:31 -0700

    Add --new flag
    Also major refactor with files models. Big improvements to unit
    testing. Update readme, gemspec description, and man page

---
+++ spec/peter_notes/files/note_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+require 'models/files/note'
+
+RSpec.describe Note do
+  let (:filename) { 'my_note.txt' }
+  context 'when initializing' do
+    context 'with dirs' do
+      it 'expands paths' do
+        dirs = '~/.secret_notes'
+        path = "#{dirs}/#{filename}"
+        expanded_dirs = '/user/home/.secret_notes'
+        expanded_path = "#{expanded_dirs}/#{filename}"
+        expect(File).to receive(:expand_path).with(path).and_return(expanded_path)
+        expect(File).to receive(:expand_path).with(dirs).and_return(expanded_dirs)
+        note = Note.new(path)
+        expect(note.path).to eq(expanded_path)
+        expect(note.dirname).to eq(expanded_dirs)
+      end
+    end
+
+    context 'without dirs' do
+      it 'doesnt expand paths' do
+
+      end
+    end
+  end
+
+  context 'when touching' do
+    it 'returns a new note' do
+      expect(FileUtils).to receive(:makedirs)
+      expect(FileUtils).to receive(:touch)
+      note = Note.touch(filename)
+      expect(note.filename).to eq(filename)
+    end
+
+    it 'touches a new note file' do
+      expect(FileUtils).to receive(:makedirs)
+      expect(FileUtils).to receive(:touch).with(filename)
+      Note.touch(filename)
+    end
+
+    it 'makes dirs if needed' do
+      dirs = '/.secret_notes/.extra_secret_notes'
+      path = "#{dirs}/#{filename}"
+      expect(FileUtils).to receive(:makedirs).with(dirs)
+      expect(FileUtils).to receive(:touch).with(path)
+      Note.touch(path)
+    end
+  end
+end
